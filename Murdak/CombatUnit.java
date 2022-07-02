b2d2b34a6e1ffc0a8a198e1b0c6d414ade99ab5b
jake00100 <105350856+jake00100@users.noreply.github.com>
2022-07-02 17:17:14 +0200

    A ver si asi si

---
+++ Murdak/CombatUnit.java
@@ -0,0 +1,73 @@
+package Murdak;
+
+import aic2022.user.*;
+
+public class CombatUnit {
+
+    UnitController uc;
+    Data data;
+    Tools tools;
+
+    void report() {
+        reportMyself();
+        //reportEnemies();
+        //reportEnvironment();
+    }
+
+    // placeholder, this is overridden for each different unit
+    void reportMyself() {
+        // Report to the Comm Channel
+        uc.writeOnSharedArray(data.unitReportCh, uc.readOnSharedArray(data.unitReportCh)+1);
+        // Reset Next Slot
+        uc.writeOnSharedArray(data.unitResetCh, 0);
+    }
+
+    void getShrine(){
+        for(ShrineInfo shrines : uc.senseShrines()){
+            if(shrines.getOwner() != data.allyTeam){
+                if(uc.canAttack()) uc.attack(shrines.getLocation());
+            }
+        }
+
+    }
+
+    void getChest(){
+        ChestInfo[] chest = uc.senseChests();
+        if (chest.length != 0){
+            Direction dir = tools.isAdjacent(chest[1].getLocation(),uc.getLocation());
+            if(dir != Direction.ZERO){
+                if(uc.canOpenChest(dir)) uc.openChest(dir);
+            }
+        }
+    }
+
+    void move(){
+        if(!Movement.doMicro() ){
+            Movement.explore();
+        }
+    }
+
+    void enterDungeon(){
+        if (uc.senseTileTypeAtLocation(uc.getLocation()) == TileType.DUNGEON) return;
+        for (entrance : uc.senseVisibleTiles(TileType.DUNGEON_ENTRANCE)){
+            Direction dir = tools.isAdjacent(entrance.get.location(), uc.getLocation());
+            if(dir != Direction.ZERO){
+                if(uc.canEnterDungeon(dir,dir)) uc.enterDungeon(dir,dir);
+            }
+        }
+    }
+
+    public int targetPriority(UnitInfo unit) {
+        if(unit.getType() == UnitType.CLERIC)       return 8;
+        if(unit.getType() == UnitType.ASSASSIN)     return 7;
+        if(unit.getType() == UnitType.MAGE)         return 6;
+        if(unit.getType() == UnitType.EXPLORER)     return 5;
+        if(unit.getType() == UnitType.RANGER)       return 4;
+        if(unit.getType() == UnitType.BARBARIAN)    return 3;
+        if(unit.getType() == UnitType.KNIGHT)       return 2;
+        if(unit.getType() == UnitType.BASE)         return 1;
+
+        return 0;
+    }
+
+}
---
+++ Murdak/Tools.java
@@ -0,0 +1,39 @@
+package Murdak;
+
+import aic2022.user.*;
+
+public class Tools {
+
+    UnitController uc;
+    Data data;
+
+    public Tools (UnitController _uc, Data _data) {
+        this.uc = _uc;
+        this.data = _data;
+    }
+
+    // Returns a random integer between 0 and n-1
+    int randomInt(int n) {
+        return (int)(Math.random()*n);
+    }
+
+    //Returns a random direction
+    Direction randomDir() {
+        int randomNum = randomInt(8);
+        return data.dirs[randomNum];
+    }
+
+    //
+    Direction isAdjacent (Location loc, Location unit){
+        if(unit.x-loc.x ==  1 && unit.x-loc.y ==  0) return Direction.EAST;
+        if(unit.x-loc.x ==  0 && unit.x-loc.y ==  1) return Direction.SOUTH;
+        if(unit.x-loc.x == -1 && unit.x-loc.y ==  0) return Direction.WEST;
+        if(unit.x-loc.x ==  0 && unit.x-loc.y == -1) return Direction.NORTH;
+        if(unit.x-loc.x ==  1 && unit.x-loc.y ==  1) return Direction.SOUTHEAST;
+        if(unit.x-loc.x ==  1 && unit.x-loc.y == -1) return Direction.NORTHEAST;
+        if(unit.x-loc.x == -1 && unit.x-loc.y ==  1) return Direction.SOUTHWEST;
+        if(unit.x-loc.x == -1 && unit.x-loc.y == -1) return Direction.NORTHWEST;
+        else return Direction.ZERO;
+    }
+
+}
